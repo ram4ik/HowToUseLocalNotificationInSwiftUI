3198c52890986c779e23ba765ff8dd493f415105
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-05 12:37:12 +0300

    Initial Commit

---
+++ HowToUseLocalNotificationInSwiftUI/HowToUseLocalNotificationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseLocalNotificationInSwiftUIApp.swift
+//  HowToUseLocalNotificationInSwiftUI
+//
+//  Created by ramil on 05.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseLocalNotificationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
